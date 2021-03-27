b3138fecfa7c9647a280dab4ea72d293d09b9259
MaryiaSiamenava <maryiasiamenava@gmail.com>
2021-03-27 13:18:53 +0300

    added fields to User and Bank

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,13 @@
 public class Bank {
+    private String name;
+
+    public Bank(String name){
+        this.name = name;
+    }
+
+    public String getNewUserId() {
+        int random = (int) (Math.random() * 1000000000);
+        String newUserId = random + "";
+        return newUserId;
+    }
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,27 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
 public class User {
+    private String firstName;
+    private String lastName;
+    private String userId;
+    private byte[] pinHash;
+    private Account account;
+    private ArrayList<Transaction> transactions;
+
+    public User(String firstName, String lastName, Bank bank, String pin) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userId = bank.getNewUserId();
+        this.account = new Account();
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            this.pinHash = md.digest(pin.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Error, NoSuchAlgorithmException");
+            System.exit(1);
+        }
+    }
 }
