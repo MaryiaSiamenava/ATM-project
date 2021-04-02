dffe153903bcc61baa18e33c5ef01bc454a90ee2
Yaroslav Paramonov <student.help.96@gmail.com>
2021-04-02 19:47:39 +0300

    fix bug - refactor IDList to Map - id vs person that holds that id

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,44 @@ import users.Admin;
 import users.Person;
 import users.User;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    private static final long SECRET_OPERATION = 7762323;
+
     public static void main(String[] args) {
         ATM atm = new ATM();
-        ArrayList<Person> IDList = new ArrayList<>();
+        Map<Long, Person> personVsIds = new HashMap<>();
 
-        Person curUser = new User ("Александр", "Александров", "1234");
+        User curUser = new User("Александр", "Александров", "1234");
         Account userAccount = new Account();
-        ((User) curUser).setAccount(userAccount);
-        System.out.println(((User) curUser).getID());
-        IDList.add(curUser);
+        curUser.setAccount(userAccount);
+        System.out.println(curUser.getID());
+        personVsIds.put(curUser.getID(), curUser);
 
-        Person curAdmin = new Admin("Николай", "Николаев", "4321");
-        System.out.println(((Admin) curAdmin).getID());
-        IDList.add(curAdmin);
+        System.out.println(curUser.getFirstName() + " " + curUser.getLastName());
+
+        Admin curAdmin = new Admin("Николай", "Николаев", "4321");
+        System.out.println(curAdmin.getID());
+        personVsIds.put(curAdmin.getID(), curAdmin);
+        System.out.println(curAdmin.getFirstName() + " " + curAdmin.getLastName());
 
         Scanner scan = new Scanner(System.in);
 
         boolean isNotSignedIn = true;
         while (isNotSignedIn) {
-            System.out.println("Введите ID пользователя.");
+            System.out.println("Введите ID пользователя: ");
             long ID = scan.nextLong();
 
-            if (IDList.contains(ID)) {
-                System.out.println("Введите ПИН-код");
+            if (personVsIds.containsKey(ID)) {
+                final Person currentPerson = personVsIds.get(ID);
+
+                System.out.println("Введите ПИН-код:");
                 for (int i = 3; i > 0; i--) {
                     String pin = scan.next();
-                    if(curUser.validateAccessCode(pin)) {
+                    if (currentPerson.validateAccessCode(pin)) {
                         System.out.println("Идентификация произведена успешно.");
                         isNotSignedIn = false;
                         break;
@@ -47,17 +55,18 @@ public class Main {
             }
         }
         isNotSignedIn = true;
-        while(isNotSignedIn) {
-            System.out.println("Выберите операцию:\n1 - пополнить счет.\n2 - снять деньги со счета.");
+        while (isNotSignedIn) {
+            System.out.println("Выберите операцию:\n1 - пополнить счет.\n2 - снять деньги со счета: ");
             int userChoice = scan.nextInt();
-            if(userChoice == 1) {
+            if (userChoice == 1) {
                 System.out.println("Введите сумму, на которую желаете пополнить счет.");
                 double amount = scan.nextDouble();
                 userAccount.addMoneyToAccount(amount);
                 System.out.println("Ваш счет пополнен на " + amount + " рублей. Текущий баланс равен " + userAccount.getBalance());
                 System.out.println("Желаете совершить другую операцию?\n1 - да\n2 - нет");
                 int kepOrQuit = scan.nextInt();
-                if(kepOrQuit == 1) {
+                if (kepOrQuit == 1) {
+                    //TODO make implementation
                 } else {
                     isNotSignedIn = false;
                     break;
@@ -72,10 +81,10 @@ public class Main {
                 } else {
                     System.out.println("На счете недостаточно денег. Текущий баланс равен " + userAccount.getBalance());
                 }
-            } else if (userChoice == 7762323) {
+            } else if (userChoice == SECRET_OPERATION) {
                 System.out.println("Здравствуйте администратор.\nВведите 1 если желаете попожить деньги в банкомат.\nВведите 2 если желаете извлечь деньги из банкомата.");
                 int adminChoice = scan.nextInt();
-                if(adminChoice == 1) {
+                if (adminChoice == 1) {
                     System.out.println("Какую сумму желаете внести?");
                     double amount = scan.nextDouble();
                     atm.putCashAmount(amount);
