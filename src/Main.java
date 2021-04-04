97d895724c7824a7a21d2893fc26102871a132b8
MaryiaSiamenava <maryiasiamenava@gmail.com>
2021-04-04 20:14:51 +0300

    added exit option to menu and for case when user failed with pin

--- src/Main.java
+++ src/Main.java
@@ -23,12 +23,14 @@ public class Main {
 
         System.out.println(curUser.getFirstName() + " " + curUser.getLastName());
 
-        Admin curAdmin = new Admin("Николай", "Николаев", "4321");
+        Admin curAdmin = new Admin("Николай", "Николаев", "123456789");
         System.out.println(curAdmin.getID());
         personVsIds.put(curAdmin.getID(), curAdmin);
         System.out.println(curAdmin.getFirstName() + " " + curAdmin.getLastName());
 
         Scanner scan = new Scanner(System.in);
+
+        atm.putCashAmount(1000000.0);
 
         boolean isNotSignedIn = true;
         while (isNotSignedIn) {
@@ -47,6 +49,7 @@ public class Main {
                         break;
                     } else {
                         System.out.println("Неверный ПИН-код. Оставшееся количество попыток: " + (i - 1));
+                        if (i == 1) System.exit(0);
                     }
                 }
 
@@ -56,19 +59,16 @@ public class Main {
         }
         isNotSignedIn = true;
         while (isNotSignedIn) {
-            System.out.println("Выберите операцию:\n1 - пополнить счет.\n2 - снять деньги со счета: ");
+            System.out.println("Выберите операцию:\n1 - пополнить счет\n2 - снять деньги со счета\n3 - перевести деньги дрйгому пользователю\n4 - выйти: ");
             int userChoice = scan.nextInt();
             if (userChoice == 1) {
-                System.out.println("Введите сумму, на которую желаете пополнить счет.");
+                System.out.println("Введите сумму, на которую желаете пополнить счет: ");
                 double amount = scan.nextDouble();
                 userAccount.addMoneyToAccount(amount);
                 System.out.println("Ваш счет пополнен на " + amount + " рублей. Текущий баланс равен " + userAccount.getBalance());
                 System.out.println("Желаете совершить другую операцию?\n1 - да\n2 - нет");
                 int kepOrQuit = scan.nextInt();
-                if (kepOrQuit == 1) {
-                    //TODO make implementation
-                } else {
-                    isNotSignedIn = false;
+                if (kepOrQuit != 1) {
                     break;
                 }
             } else if (userChoice == 2) {
@@ -78,21 +78,30 @@ public class Main {
                     curUser.takeMoneyFromATM(atm, amount);
                     userAccount.takeMoneyFromAccount(amount);
                     System.out.println("Операция произведена успешно. Остаток на счете равен " + userAccount.getBalance());
+                    System.out.println("Желаете совершить другую операцию?\n1 - да\n2 - нет");
+                    int kepOrQuit = scan.nextInt();
+                    if (kepOrQuit != 1) {
+                        break;
+                    }
                 } else {
                     System.out.println("На счете недостаточно денег. Текущий баланс равен " + userAccount.getBalance());
                 }
-            } else if (userChoice == SECRET_OPERATION) {
-                System.out.println("Здравствуйте администратор.\nВведите 1 если желаете попожить деньги в банкомат.\nВведите 2 если желаете извлечь деньги из банкомата.");
+            } else if (userChoice == 3) {
+                //TODO implementation
+            } else if (userChoice == 4) {
+                break;
+            } else if ( userChoice == SECRET_OPERATION) {
+                System.out.println("Здравствуйте администратор.\nВведите 1 если желаете попожить деньги в банкомат\nВведите 2 если желаете извлечь деньги из банкомата: ");
                 int adminChoice = scan.nextInt();
                 if (adminChoice == 1) {
-                    System.out.println("Какую сумму желаете внести?");
+                    System.out.println("Введите сумму, которую желаете внести: ");
                     double amount = scan.nextDouble();
                     atm.putCashAmount(amount);
                     System.out.println("Операция произведена успешно. Текущий запас наличных средств: " + atm.getBalance());
                 } else if (adminChoice == 2) {
-                    System.out.println("Какую сумму желаете извлечь?");
+                    System.out.println("Введите сумму, которую желаете извлечь: ");
                     double amount = scan.nextDouble();
-                    atm.putCashAmount(amount);
+                    curAdmin.takeMoneyFromATM(atm, amount);
                     System.out.println("Операция произведена успешно. Текущий запас наличных средств: " + atm.getBalance());
                 }
             }
